81f75671a069ee98f02eb5a21f17b8e3ebf9b698
sylvain-corre <sylvain.corre@orange.fr>
2016-02-10 13:36:41 +0100

    Better use of HAL library

--- STM32F7_led_blink/src/main.c
+++ STM32F7_led_blink/src/main.c
@@ -11,20 +11,6 @@
 #include "stm32f7xx.h"
 #include "stm32746g_discovery.h"
 
-/**************/
-/*** led_on ***/
-/**************/
-static void led_on() {
-	HAL_GPIO_WritePin(LED1_GPIO_PORT, LED1_PIN, GPIO_PIN_SET);
-}
-
-/***************/
-/*** led_off ***/
-/***************/
-static void led_off() {
-	HAL_GPIO_WritePin(LED1_GPIO_PORT, LED1_PIN, GPIO_PIN_RESET);
-}
-
 /****************/
 /*** init_led ***/
 /****************/
@@ -39,34 +25,26 @@ static void init_led() {
 	gpioInitStructure.Speed = GPIO_SPEED_HIGH;
 	HAL_GPIO_Init(LED1_GPIO_PORT, &gpioInitStructure);
 
-	led_off();
-}
-
-/*************/
-/*** pause ***/
-/*************/
-static void pause() {
-	unsigned int i;
-
-	for (i = 0; i < 1000000; i++)
-		;
+	/* Switch off led */
+	HAL_GPIO_WritePin(LED1_GPIO_PORT, LED1_PIN, GPIO_PIN_RESET);
 }
 
 /************/
 /*** main ***/
 /************/
 int main(void) {
+	/*** Init ***/
+	HAL_Init();
+
 	/* Init LED GPIO */
 	init_led();
 
 	for (;;) {
-		/* Switch on the led */
-		led_on();
-		pause();
+		/* Switch led state */
+		HAL_GPIO_TogglePin(LED1_GPIO_PORT, LED1_PIN);
 
-		/* Switch off the led */
-		led_off();
-		pause();
+		/* Wait 1s */
+		HAL_Delay(1000);
 	}
 
 	/* NotReached */
